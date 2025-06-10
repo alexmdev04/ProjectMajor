cc9cdd16174887612c9da8039945812369f962fa
Alex M <alexmdev04@gmail.com>
2025-06-10 08:34:51 +0100

    Add triggerable bounce pad

---
+++ Assets/Scripts/World/Triggerables/BouncePad.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Major.World {
+    public class BouncePad : Triggerable {
+        [SerializeField] private float strength = 100.0f;
+        [SerializeField] private Vector3 direction = new Vector3(0.0f, 1.0f, 1.0f);
+        protected override void OnTriggered(Trigger senderTrigger, GameObject sender) {
+            if (sender.TryGetComponent<Collider>(out var senderCollider)) {
+                var senderRb = senderCollider.attachedRigidbody;
+                if (senderRb) {
+                    senderRb.AddForceAtPosition(strength * direction, transform.position, ForceMode.Impulse);
+                }
+            }
+        }
+        protected override void OnUntriggered(Trigger senderTrigger, GameObject sender) {
+
+        }
+    }
+}
