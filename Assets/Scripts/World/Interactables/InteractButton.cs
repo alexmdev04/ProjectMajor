b0733b87b52c4e596a386f460cea470e91f3f9f7
Alex M <alexmdev04@gmail.com>
2025-06-10 08:35:52 +0100

    Add interactable button (press F button)

---
+++ Assets/Scripts/World/Interactables/InteractButton.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Major.World {
+    [RequireComponent(typeof(CustomTrigger))]
+    public class InteractButton : Interactable {
+        [SerializeField] private SlideAnimation anim;
+        [SerializeField] private float buttonTimer = 1.0f;
+        private CustomTrigger customTrigger;
+        private bool pressed;
+
+        private void Awake() {
+            customTrigger = GetComponent<CustomTrigger>();
+            customTrigger.onBegin += (s) => {
+                anim.SetAnimationState(true);
+                pressed = true;
+            };
+            customTrigger.onEnd += (s) => {
+                anim.SetAnimationState(false);
+                pressed = false;
+            };
+        }
+
+        public override void Interact(Player sender, Action callback = null) {
+            if (!pressed) {
+                StartCoroutine(ButtonTimer(sender.gameObject));
+            }
+        }
+
+        private IEnumerator ButtonTimer(GameObject sender) {
+            customTrigger.Begin(sender);
+
+            yield return new WaitForSeconds(buttonTimer);
+
+            customTrigger.End(sender);
+        }
+    }
+}
