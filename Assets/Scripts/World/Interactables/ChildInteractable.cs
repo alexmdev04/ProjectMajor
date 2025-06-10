c0d94da836fa0d730eb691bfeb02e14db3adb790
Alex M <alexmdev04@gmail.com>
2025-06-10 08:27:29 +0100

    Create ChildInteractable

---
+++ Assets/Scripts/World/Interactables/ChildInteractable.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace Major.World {
+    public class ChildInteractable : Interactable {
+        public Interactable parent;
+        private void Awake() {
+            if (parent == null) {
+                parent = transform.parent.GetComponent<Interactable>();
+            }
+        }
+        public override void Interact(Player sender, Action callback = null) {
+            parent.Interact(sender, callback);
+        }
+    }
+}
