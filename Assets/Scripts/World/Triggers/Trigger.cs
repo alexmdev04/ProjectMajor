abb12141cbe2d04e696f3415f1ab911e24c5664e
Alex M <alexmdev04@gmail.com>
2025-06-19 03:53:49 +0100

    Triggers can now trigger multiple triggerables

--- Assets/Scripts/World/Triggers/Trigger.cs
+++ Assets/Scripts/World/Triggers/Trigger.cs
@@ -1,16 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Major.World {
     public abstract class Trigger : MonoBehaviour {
-        [SerializeField] private Triggerable triggerable;
+        [SerializeField] private List<Triggerable> triggerables = new();
         public void Begin(GameObject sender) {
-            triggerable.Begin(this, sender);
+            foreach (var triggerable in triggerables) {
+                triggerable.Begin(this, sender);            
+            }
             OnTriggerBegin(sender);
         }
         protected abstract void OnTriggerBegin(GameObject sender);
 
         public void End(GameObject sender) {
-            triggerable.End(this, sender);
+            foreach (var triggerable in triggerables) {
+                triggerable.End(this, sender);            
+            }
             OnTriggerEnd(sender);
         }
         protected abstract void OnTriggerEnd(GameObject sender);
