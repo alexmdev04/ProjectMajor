6cdc0c7c0e52935138380a1dad3e55623c65f1fd
Alex M <alexmdev04@gmail.com>
2025-06-24 04:16:32 +0100

    Object killer now has kill sender param

--- Assets/Scripts/World/Triggerables/TriggerableObjectKiller.cs
+++ Assets/Scripts/World/Triggerables/TriggerableObjectKiller.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 namespace Major.World {
     public class TriggerableObjectKiller : Triggerable {
         public GameObject obj;
+        [SerializeField] private bool killSender;
 
         protected override void OnTriggered(Trigger senderTrigger, GameObject sender) {
-            if (obj == Player.instance.gameObject) {
+            var target = killSender ? sender : obj;
+
+            if (target == Player.instance.gameObject) {
                 GameManager.instance.OnPlayerKilled();
                 return;
             }
-            if (obj == Kevin.instance.gameObject) {
+
+            if (target == Kevin.instance.gameObject) {
                 GameManager.instance.OnKevinKilled();
                 return;
             }
-            Destroy(obj);
+            
+            Destroy(target);
         }
 
         protected override void OnUntriggered(Trigger senderTrigger, GameObject sender) {
