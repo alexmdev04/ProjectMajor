62a68ed9fedbdb94d2a3d867ae9d7dbe22634621
Alex M <alexmdev04@gmail.com>
2025-06-26 04:30:37 +0100

    Add noclip and paused states to debug text

--- Assets/Scripts/Debug/Stats.cs
+++ Assets/Scripts/Debug/Stats.cs
@@ -13,12 +13,18 @@ namespace Major.Debug {
 
         // Update is called once per frame
         private void Update() {
-            var text = new StringBuilder("FPS: ").Append((int)(1.0f / Time.deltaTime)).Append("\n");
+            var text = new StringBuilder("FPS: ").Append((int)(1.0f / Time.unscaledDeltaTime)).Append("\n");
             if (Player.instance) {
                 text.Append("Speed: ").Append(MathF.Round(Player.instance.rb.linearVelocity.magnitude, 4)).Append("\n");                
             }
             if (LevelManager.levelCurrent) {
                 text.Append("Level: ").Append(LevelManager.levelCurrent.levelAsset.name).Append("\n");
+            }
+            if (GameManager.instance.dbg_noclipEnabled) {
+                text.Append("Noclip\n");
+            }
+            if (GameManager.instance.paused) {
+                text.Append("Paused\n");
             }
             textBox.text = text.ToString();
         }
