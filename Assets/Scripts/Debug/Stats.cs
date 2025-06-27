567a680873deefecd15aeb972a316e1ad418d455
Alex M <alexmdev04@gmail.com>
2025-06-27 21:18:19 +0100

    Add current checkpoint to debug stats

--- Assets/Scripts/Debug/Stats.cs
+++ Assets/Scripts/Debug/Stats.cs
@@ -15,10 +15,13 @@ namespace Major.Debug {
         private void Update() {
             var text = new StringBuilder("FPS: ").Append((int)(1.0f / Time.unscaledDeltaTime)).Append("\n");
             if (Player.instance) {
-                text.Append("Speed: ").Append(MathF.Round(Player.instance.rb.linearVelocity.magnitude, 4)).Append("\n");                
+                text.Append("Speed: ").Append(MathF.Round(Player.instance.rb.linearVelocity.magnitude, 4)).Append("\n");
             }
             if (LevelManager.levelCurrent) {
                 text.Append("Level: ").Append(LevelManager.levelCurrent.levelAsset.name).Append("\n");
+                if (LevelManager.levelCurrent.checkpointCurrent) {
+                    text.Append("Checkpoint: ").Append(LevelManager.levelCurrent.checkpointCurrent.name).Append("\n");
+                }
             }
             if (GameManager.instance.dbg_noclipEnabled) {
                 text.Append("Noclip\n");
