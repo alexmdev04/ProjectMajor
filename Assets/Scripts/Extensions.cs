1c9f3c1a258629f31d9d7afc24309983c21c66a9
Alex M <alexmdev04@gmail.com>
2025-07-04 21:12:18 +0100

    Add multiply vector3 ext function

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -106,5 +106,13 @@ namespace Major {
                 RoundToVal(val1.z, val2.z)
             );
         }
+
+        public static Vector3 Multiply(Vector3 val1, Vector3 val2) {
+            return new (
+                val1.x * val2.x,
+                val1.y * val2.y,
+                val1.z * val2.z
+            );
+        }
     }
 }
