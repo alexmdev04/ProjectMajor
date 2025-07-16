c23ed72d9536d56d81571ce1c05d4cebd0f774e0
Alex M <alexmdev04@gmail.com>
2025-07-16 04:11:20 +0100

    Interactable trigger can now toggle, and is optimised

--- Assets/Scripts/World/Interactables/InteractableTrigger.cs
+++ Assets/Scripts/World/Interactables/InteractableTrigger.cs
@@ -6,32 +6,57 @@ namespace Major.World {
     [RequireComponent(typeof(CustomTrigger))]
     public class InteractableTrigger : Interactable {
         [SerializeField] private float timer = 1.0f;
+        [SerializeField] private bool toggleable = false;
         private CustomTrigger customTrigger;
         private bool active;
         [SerializeField] private string prompt = "Interact";
+        private Coroutine timerCoroutine;
 
         private void Awake() {
             customTrigger = GetComponent<CustomTrigger>();
-            customTrigger.onBegin += (s) => {
-                active = true;
-            };
-            customTrigger.onEnd += (s) => {
-                active = false;
-            };
         }
 
         public override void Interact(Player sender, Action callback = null) {
-            if (!active) {
-                StartCoroutine(Timer(sender.gameObject));
+            if (active) {
+                if (toggleable) {
+                    End(sender.gameObject);
+                }
+                else {
+                    ResetTimer(sender.gameObject);
+                }
+            }
+            else {
+                Begin(sender.gameObject);
             }
         }
 
-        private IEnumerator Timer(GameObject sender) {
+        private void Begin(GameObject sender) {
             customTrigger.Begin(sender);
+            active = true;
+            if (timer != 0.0f) {
+                timerCoroutine = StartCoroutine(Timer(sender));
+            }
 
+            customTrigger.Begin(sender);
+            active = true;
+        }
+
+        private void ResetTimer(GameObject sender) {
+            if (timer != 0.0f) {
+                StopCoroutine(timerCoroutine);
+                timerCoroutine = StartCoroutine(Timer(sender));
+            }
+        } 
+
+        private void End(GameObject sender) {
+            customTrigger.End(sender);
+            active = false;
+        }
+
+        private IEnumerator Timer(GameObject sender) {
             yield return new WaitForSeconds(timer);
 
-            customTrigger.End(sender);
+            End(sender);
         }
 
         public override string GetPrompt() {
