0aa185f09d9c1fa010e1bd5568dcf33db72efdeb
Alex M <alexmdev04@gmail.com>
2025-07-16 04:13:09 +0100

    Conveyor belt can now be triggered to change direction, and added prefab

--- Assets/Scripts/World/ConveyorBelt.cs
+++ Assets/Scripts/World/ConveyorBelt.cs
@@ -2,15 +2,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Major.World {
+    [RequireComponent(typeof(CustomTriggerable))]
     public class ConveyorBelt : MonoBehaviour {
         [SerializeField] private Vector3 direction = Vector3.back;
+        [SerializeField] private Vector3 triggeredDirection = Vector3.zero;
+        private Vector3 currentDirection = Vector3.zero;
         [SerializeField] private float strength = 1.0f;
         [SerializeField] private HashSet<Rigidbody> targetObjects = new();
         [SerializeField] private ForceMode forceMode = ForceMode.VelocityChange;
+        private CustomTriggerable customTriggerable;
+
+        private void Awake() {
+            currentDirection = direction;
+
+            if (triggeredDirection == Vector3.zero) {
+                triggeredDirection = -direction;
+            }
+
+            customTriggerable = GetComponent<CustomTriggerable>();
+
+            customTriggerable.onTriggered += (senderTrigger, sender) => {
+                currentDirection = triggeredDirection;
+            };
+
+            customTriggerable.onUntriggered += (senderTrigger, sender) => {
+                currentDirection = direction;
+            };
+        }
 
         private void FixedUpdate() {
             foreach (var obj in targetObjects) {
-                obj.AddForce(strength * direction, forceMode);
+                obj.AddForce(strength * currentDirection, forceMode);
             }
         }
 
@@ -25,7 +47,7 @@ namespace Major.World {
             if (!sender.attachedRigidbody) {
                 return;
             }
-            targetObjects.Remove(sender.attachedRigidbody);            
+            targetObjects.Remove(sender.attachedRigidbody);
         }
     }
 }
